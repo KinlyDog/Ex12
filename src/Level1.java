718c5d0fd3ea40f1745bd61163e9e2933e9b03a5
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-15 06:09:46 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -1,16 +1,26 @@
 public class Level1 {
     public static boolean MisterRobot(int n, int[] data) {
-        long start = System.currentTimeMillis();
         int[] ma = new int[n];
+        int[] tmp = new int[n];
 
         for (int i = 0; i < n; i++) {
             ma[i] = data[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (ma[i] != i + 1) break;
+
+            if (i == n - 1) return true;
         }
 
         boolean flag = false;
 
         while (!flag) {
             flag = true;
+
+            for (int i = 0; i < n; i++) {
+                tmp[i] = ma[i];
+            }
 
             for (int i = 0; i < n - 2; i++) {
                 for (int j = 0; j < 3; j++) {
@@ -22,15 +32,28 @@ public class Level1 {
                     ma[0 + i] = ma[1 + i];
                     ma[1 + i] = ma[2 + i];
                     ma[2 + i] = x;
-
-                    flag = false;
                 }
             }
 
-            long finish = System.currentTimeMillis();
-            long dif = finish - start;
+            for (int i = 0; i < n; i++) {
+                if (ma[i] != i + 1) {
+                    flag = false;
+                    break;
+                }
 
-            if (dif > 1000) return false;
+                if (i == n - 1) return true;
+            }
+
+
+            for (int i = 0; i < n; i++) {
+                if (tmp[i] != ma[i]) {
+                    break;
+                }
+
+                if (i == n - 1) {
+                    return false;
+                }
+            }
         }
 
         return flag;
