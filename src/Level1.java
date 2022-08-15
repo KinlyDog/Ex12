9d2dd83b7ea6f9b6ee17f0f0b22cb8b055e946f6
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-16 02:53:15 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -1,16 +1,9 @@
 public class Level1 {
     public static boolean MisterRobot(int n, int[] data) {
         int[] ma = new int[n];
-        int[] tmp = new int[n];
 
         for (int i = 0; i < n; i++) {
             ma[i] = data[i];
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (ma[i] != i + 1) break;
-
-            if (i == n - 1) return true;
         }
 
         boolean flag = false;
@@ -18,40 +11,18 @@ public class Level1 {
         while (!flag) {
             flag = true;
 
-            for (int i = 0; i < n; i++) {
-                tmp[i] = ma[i];
-            }
-
             for (int i = 0; i < n - 2; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (ma[0 + i] < ma[1 + i] && ma[1 + i] < ma[2 + i]) {
+                    if (ma[i] < ma[i + 1] && ma[i + 1] < ma[i + 2]) {
                         break;
                     }
 
-                    int x = ma[0 + i];
-                    ma[0 + i] = ma[1 + i];
-                    ma[1 + i] = ma[2 + i];
-                    ma[2 + i] = x;
-                }
-            }
+                    int x = ma[i];
+                    ma[i] = ma[i + 1];
+                    ma[i + 1] = ma[i + 2];
+                    ma[i + 2] = x;
 
-            for (int i = 0; i < n; i++) {
-                if (ma[i] != i + 1) {
-                    flag = false;
-                    break;
-                }
-
-                if (i == n - 1) return true;
-            }
-
-
-            for (int i = 0; i < n; i++) {
-                if (tmp[i] != ma[i]) {
-                    break;
-                }
-
-                if (i == n - 1) {
-                    return false;
+                    if (j == 2) return false;
                 }
             }
         }
