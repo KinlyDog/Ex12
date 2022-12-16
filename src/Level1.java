35083378b55ec47f288bdcdc71f2875c867ada6a
Andrey Otryaskin <kinlydog@gmail.com>
2022-12-16 22:42:46 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -1,32 +1,30 @@
 public class Level1 {
     public static boolean MisterRobot(int n, int[] data) {
-        int[] ma = new int[n];
+        int[] sortedData = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            ma[i] = data[i];
-        }
+        System.arraycopy(data, 0, sortedData, 0, n);
 
-        boolean flag = false;
+        boolean isSorted = false;
 
-        while (!flag) {
-            flag = true;
+        while (!isSorted) {
+            isSorted = true;
 
             for (int i = 0; i < n - 2; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (ma[i] < ma[i + 1] && ma[i + 1] < ma[i + 2]) {
+                    if (sortedData[i] < sortedData[i + 1] && sortedData[i + 1] < sortedData[i + 2]) {
                         break;
                     }
 
-                    int x = ma[i];
-                    ma[i] = ma[i + 1];
-                    ma[i + 1] = ma[i + 2];
-                    ma[i + 2] = x;
+                    int x = sortedData[i];
+                    sortedData[i] = sortedData[i + 1];
+                    sortedData[i + 1] = sortedData[i + 2];
+                    sortedData[i + 2] = x;
 
-                    if (j == 2) return false;
+                    if (j > 1) return false;
                 }
             }
         }
 
-        return flag;
+        return true;
     }
 }
